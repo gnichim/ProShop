e035d2477f5000be40a36758cd33207b779b71ed
mohamed Gnichi <mohamed.Gnichi@soglu.internal>
2023-07-28 16:49:55 +0200

    fifth commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,15 +5,19 @@ import {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
+  deleteUser,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 // to protect this route we add protect middleware as first parameter
 router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+router.route("/:id").delete(protect, admin, deleteUser);
 
 export default router;
